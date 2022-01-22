df420f09ff7495b0e67f0e99f5216554ff0ec3b0
Gowtham G <webappsbygowtham@gmail.com>
2022-01-22 22:21:14 +0530

    Adjust header and form to support mobile screen

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => (
-  <header className="w-full top-0 left-0 px-16 py-6 bg-blue-500 shadow-md">
+  <header className="w-full top-0 left-0 px-4 py-4 md:px-12 bg-blue-500 shadow-md">
     <div className="flex items-center justify-between">
-      <a href="/" className="block font-medium text-3xl text-gray-900 ">
+      <a href="/" className="block font-medium text-2xl md:text-3xl text-gray-900 ">
         Hailstone {"{N}"}
       </a>
     </div>
